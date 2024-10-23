91f802d7bb83655454784f852e8103fc37e6a041
Agnish Paul <97803357+Agnish77@users.noreply.github.com>
2024-10-23 22:27:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFDirectoryLoader
-
+###
 
 from dotenv import load_dotenv
 load_dotenv()
